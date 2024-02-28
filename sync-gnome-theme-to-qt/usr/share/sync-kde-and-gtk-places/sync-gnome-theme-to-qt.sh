b9d1ae08666668c8e7b717f8425e1638fecb3ffa
Bruno Gonçalves <bigbruno@gmail.com>
2024-02-28 05:16:44 -0300

    Possible automatic change fonts

--- sync-gnome-theme-to-qt/usr/share/sync-kde-and-gtk-places/sync-gnome-theme-to-qt.sh
+++ sync-gnome-theme-to-qt/usr/share/sync-kde-and-gtk-places/sync-gnome-theme-to-qt.sh
@@ -4,7 +4,62 @@
 # By Bruno Goncalves < www.biglinux.com.br >
 # Create in: 2023/02/08
 
+# Change kdeglobals fonts based in gnome font configuration
+# Commented because qt automatic detect, but if using desktop without automatic detect
+# Just uncomment next lines
+# # Read Gnome font config and convert in variables
+# while IFS= read -r Line; do
+#     # if [[ $Line =~ ^document-font-name= ]]; then
+#         #document_font_name=${Line/*=}
+#         # document_font_name=${document_font_name//\'}
+#         # document_font_size=${document_font_name##* }
+#         # document_font_name=${document_font_name% *}
+#         # document_font_style=${document_font_name##* }
+#         # echo $document_font_size
+#         # echo $document_font_name
+#         # echo $document_font_style
+# 
+#     if [[ $Line =~ ^font-name= ]]; then
+#         font_name=${Line/*=}
+#         font_name=${font_name//\'}
+#         font_size=${font_name##* }
+#         font_name=${font_name% *}
+#         font_style=${font_name##* }
+#         # echo $font_size
+#         # echo $font_name
+#         # echo $font_style
+#         
+#     elif [[ $Line =~ ^monospace-font-name= ]]; then
+#         monospace_font_name=${Line/*=}
+#         monospace_font_name=${monospace_font_name//\'}
+#         monospace_font_size=${monospace_font_name##* }
+#         monospace_font_name=${monospace_font_name% *}
+#         monospace_font_style=${monospace_font_name##* }
+#         # echo $monospace_font_size
+#         # echo $monospace_font_name
+#         # echo $monospace_font_style
+# 
+#     elif [[ $Line =~ ^titlebar-font= ]]; then
+#         titlebar_font_name=${Line/*=}
+#         titlebar_font_name=${titlebar_font_name//\'}
+#         titlebar_font_size=${titlebar_font_name##* }
+#         titlebar_font_name=${titlebar_font_name% *}
+#         titlebar_font_style=${titlebar_font_name##* }
+#         # echo $titlebar_font_size
+#         # echo $titlebar_font_name
+#         # echo $titlebar_font_style    
+#     fi
+# done <<< "$(dconf dump /org/gnome/desktop/ | grep -e 'document-font-name=' -e 'font-name=' -e 'monospace-font-name=' -e 'titlebar-font=')"
+# 
+# # Apply font changes in kdeglobals file
+# sed -i "s|^fixed=.*|fixed=$monospace_font_name,$monospace_font_size,-1,5,50,0,0,0,0,0,$monospace_font_style|g; \
+#     s|^font=.*|font=$font_name,$font_size,-1,5,63,0,0,0,0,0,$font_style|g; \
+#     s|^menuFont=.*|menuFont=$font_name,$font_size,-1,5,63,0,0,0,0,0,$font_style|g; \
+#     s|^smallestReadableFont=.*|smallestReadableFont=$font_name,$font_size,-1,5,63,0,0,0,0,0,$font_style|g; \
+#     s|^toolBarFont=.*|toolBarFont=$font_name,$font_size,-1,5,63,0,0,0,0,0,$font_style|g; \
+#     s|^activeFont=.*|activeFont=$titlebar_font_name,$titlebar_font_size,-1,5,63,0,0,0,0,0,$titlebar_font_style|g" ~/.config/kdeglobals
 
+    
 ColorScheme="$(dconf read /org/gnome/desktop/interface/color-scheme)"
 KvantumTheme="$(grep 'theme=' ~/.config/Kvantum/kvantum.kvconfig)"
 
