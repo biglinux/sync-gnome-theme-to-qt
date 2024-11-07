4e601b114d5d56cc150f73d4ffe55203bfb8fc1c
Bruno Gonçalves <bigbruno@gmail.com>
2024-11-06 22:12:24 -0300

    Add support for Cinnamon

--- sync-gnome-theme-to-qt/usr/share/sync-kde-and-gtk-places/sync-gnome-theme-to-qt.sh
+++ sync-gnome-theme-to-qt/usr/share/sync-kde-and-gtk-places/sync-gnome-theme-to-qt.sh
@@ -1,70 +1,36 @@
 #!/bin/sh
-#
-# From gtk gnome theme to kvantum theme
-# By Bruno Goncalves < www.biglinux.com.br >
-# Create in: 2023/02/08
 
-# Change kdeglobals fonts based in gnome font configuration
-# Commented because qt automatic detect, but if using desktop without automatic detect
-# Just uncomment next lines
-# # Read Gnome font config and convert in variables
-# while IFS= read -r Line; do
-#     # if [[ $Line =~ ^document-font-name= ]]; then
-#         #document_font_name=${Line/*=}
-#         # document_font_name=${document_font_name//\'}
-#         # document_font_size=${document_font_name##* }
-#         # document_font_name=${document_font_name% *}
-#         # document_font_style=${document_font_name##* }
-#         # echo $document_font_size
-#         # echo $document_font_name
-#         # echo $document_font_style
-# 
-#     if [[ $Line =~ ^font-name= ]]; then
-#         font_name=${Line/*=}
-#         font_name=${font_name//\'}
-#         font_size=${font_name##* }
-#         font_name=${font_name% *}
-#         font_style=${font_name##* }
-#         # echo $font_size
-#         # echo $font_name
-#         # echo $font_style
-#         
-#     elif [[ $Line =~ ^monospace-font-name= ]]; then
-#         monospace_font_name=${Line/*=}
-#         monospace_font_name=${monospace_font_name//\'}
-#         monospace_font_size=${monospace_font_name##* }
-#         monospace_font_name=${monospace_font_name% *}
-#         monospace_font_style=${monospace_font_name##* }
-#         # echo $monospace_font_size
-#         # echo $monospace_font_name
-#         # echo $monospace_font_style
-# 
-#     elif [[ $Line =~ ^titlebar-font= ]]; then
-#         titlebar_font_name=${Line/*=}
-#         titlebar_font_name=${titlebar_font_name//\'}
-#         titlebar_font_size=${titlebar_font_name##* }
-#         titlebar_font_name=${titlebar_font_name% *}
-#         titlebar_font_style=${titlebar_font_name##* }
-#         # echo $titlebar_font_size
-#         # echo $titlebar_font_name
-#         # echo $titlebar_font_style    
-#     fi
-# done <<< "$(dconf dump /org/gnome/desktop/ | grep -e 'document-font-name=' -e 'font-name=' -e 'monospace-font-name=' -e 'titlebar-font=')"
-# 
-# # Apply font changes in kdeglobals file
-# sed -i "s|^fixed=.*|fixed=$monospace_font_name,$monospace_font_size,-1,5,50,0,0,0,0,0,$monospace_font_style|g; \
-#     s|^font=.*|font=$font_name,$font_size,-1,5,63,0,0,0,0,0,$font_style|g; \
-#     s|^menuFont=.*|menuFont=$font_name,$font_size,-1,5,63,0,0,0,0,0,$font_style|g; \
-#     s|^smallestReadableFont=.*|smallestReadableFont=$font_name,$font_size,-1,5,63,0,0,0,0,0,$font_style|g; \
-#     s|^toolBarFont=.*|toolBarFont=$font_name,$font_size,-1,5,63,0,0,0,0,0,$font_style|g; \
-#     s|^activeFont=.*|activeFont=$titlebar_font_name,$titlebar_font_size,-1,5,63,0,0,0,0,0,$titlebar_font_style|g" ~/.config/kdeglobals
+# Function to check if current theme is dark
+is_dark_theme() {
+    if [[ "$XDG_CURRENT_DESKTOP" = *"Cinnamon" ]]; then
+        local current_theme=$(dconf read /org/cinnamon/desktop/interface/gtk-theme)
+        # Remove quotes from theme name
+        current_theme=${current_theme//\'/}
+        
+        # Check if it's a Big- theme
+        if [[ "$current_theme" == Big-* ]]; then
+            # If doesn't have Light, it's dark
+            [[ "$current_theme" != *Light ]] && return 0
+            return 1
+        else
+            # For other themes, check for dark in name
+            [[ "$current_theme" == *dark* ]] && return 0
+            return 1
+        fi
+    else
+        # GNOME color scheme check
+        local color_scheme=$(dconf read /org/gnome/desktop/interface/color-scheme)
+        [[ "$color_scheme" = "'prefer-dark'" ]] && return 0
+        return 1
+    fi
+}
 
 # Only change if not in KDE
 if [[ "$XDG_SESSION_DESKTOP" != "KDE" ]]; then
-    ColorScheme="$(dconf read /org/gnome/desktop/interface/color-scheme)"
     KvantumTheme="$(grep 'theme=' ~/.config/Kvantum/kvantum.kvconfig)"
 
-    if [ "$ColorScheme" = "'prefer-dark'" -a "$KvantumTheme" != "theme=BigAdwaitaRoundDark" ]; then
+    # Dark theme configuration
+    if is_dark_theme && [ "$KvantumTheme" != "theme=BigAdwaitaRoundDark" ]; then
         IconTheme="$(dconf read /org/gnome/desktop/interface/icon-theme)"
         mkdir -p ~/.config/Kvantum/
         echo '[General]
@@ -73,34 +39,44 @@ if [[ "$XDG_SESSION_DESKTOP" != "KDE" ]]; then
         # Copy the configuration file for the dark theme
         cp -f /usr/share/sync-kde-and-gtk-places/biglinux-dark ~/.config/kdeglobals
 
+        # Process and set dark icon theme
         IconFolder="$(ls -d /usr/share/icons/*/ ~/.local/share/icons/*/ 2> /dev/null | grep -i dark | grep -im1 "/${IconTheme//\'/}")"
         IconFolderClean1=${IconFolder%/}
         IconFolderClean2=${IconFolderClean1##*/}
         if [ "$IconFolderClean2" != "" ]; then
-            dconf write /org/gnome/desktop/interface/icon-theme "'$IconFolderClean2'"
+            if [[ "$XDG_CURRENT_DESKTOP" = *"Cinnamon" ]]; then
+                dconf write /org/cinnamon/desktop/interface/icon-theme "'$IconFolderClean2'"
+            else
+                dconf write /org/gnome/desktop/interface/icon-theme "'$IconFolderClean2'"
+            fi
         fi
 
         exit 0
     fi
 
-    if [ "$ColorScheme" != "'prefer-dark'" -a "$KvantumTheme" != "theme=BigAdwaitaRound" ]; then
+    # Light theme configuration
+    if ! is_dark_theme && [ "$KvantumTheme" != "theme=BigAdwaitaRound" ]; then
         IconTheme="$(dconf read /org/gnome/desktop/interface/icon-theme)"
         mkdir -p ~/.config/Kvantum/
         echo '[General]
     theme=BigAdwaitaRoundGtk' > ~/.config/Kvantum/kvantum.kvconfig
 
-        # Copy the configuration file for the non-dark theme
+        # Copy the configuration file for the light theme
         cp -f /usr/share/sync-kde-and-gtk-places/biglinux ~/.config/kdeglobals
 
+        # Process and set light icon theme
         IconThemeWithoutDark="$(echo $IconTheme | sed 's|-dark||gi;s|dark||gi')"
         IconFolder="$(ls -d /usr/share/icons/*/ ~/.local/share/icons/*/ 2> /dev/null | grep -vi dark | grep -im1 "/${IconThemeWithoutDark//\'/}")"
         IconFolderClean1=${IconFolder%/}
         IconFolderClean2=${IconFolderClean1##*/}
         if [ "$IconFolderClean2" != "" ]; then
-            dconf write /org/gnome/desktop/interface/icon-theme "'$IconFolderClean2'"
+            if [[ "$XDG_CURRENT_DESKTOP" = *"Cinnamon" ]]; then
+                dconf write /org/cinnamon/desktop/interface/icon-theme "'$IconFolderClean2'"
+            else
+                dconf write /org/gnome/desktop/interface/icon-theme "'$IconFolderClean2'"
+            fi
         fi
 
         exit 0
     fi
-
 fi
