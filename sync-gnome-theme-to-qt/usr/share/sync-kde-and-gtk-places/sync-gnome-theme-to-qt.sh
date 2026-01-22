f53ceee0b47a4396193dcd0e23d461d6c0225076
talesam <talesam@gmail.com>
2026-01-22 13:32:56 -0300

    🔨 refactor: Simplifying the code to switch the theme of QT-GTK apps from light to dark.

--- sync-gnome-theme-to-qt/usr/share/sync-kde-and-gtk-places/sync-gnome-theme-to-qt.sh
+++ sync-gnome-theme-to-qt/usr/share/sync-kde-and-gtk-places/sync-gnome-theme-to-qt.sh
@@ -36,18 +36,6 @@ if [[ "$XDG_SESSION_DESKTOP" != "KDE" ]]; then
 
     # Dark theme configuration
     if is_dark_theme && [ "$KvantumTheme" != "theme=BigAdwaitaRoundDark" ]; then
-        # Configure GTK4 dark theme
-        if [[ "$XDG_CURRENT_DESKTOP" = *"Cinnamon" ]] || [[ "$XDG_CURRENT_DESKTOP" == "XFCE" ]] || [[ "$XDG_SESSION_DESKTOP" == "xfce" ]]; then
-            dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
-        fi
-        
-        # Get icon theme based on desktop environment
-        if [[ "$XDG_CURRENT_DESKTOP" == "XFCE" ]] || [[ "$XDG_SESSION_DESKTOP" == "xfce" ]]; then
-            IconTheme="$(xfconf-query -c xsettings -p /Net/IconThemeName 2>/dev/null)"
-        else
-            IconTheme="$(dconf read /org/gnome/desktop/interface/icon-theme)"
-        fi
-        
         mkdir -p ~/.config/Kvantum/
         echo '[General]
     theme=BigAdwaitaRoundGtkDark' > ~/.config/Kvantum/kvantum.kvconfig
@@ -55,58 +43,17 @@ if [[ "$XDG_SESSION_DESKTOP" != "KDE" ]]; then
         # Copy the configuration file for the dark theme
         cp -f /usr/share/sync-kde-and-gtk-places/biglinux-dark ~/.config/kdeglobals
 
-        # Process and set dark icon theme
-        IconFolder="$(ls -d /usr/share/icons/*/ ~/.local/share/icons/*/ 2> /dev/null | grep -i dark | grep -im1 "/${IconTheme//\'/}")"
-        IconFolderClean1=${IconFolder%/}
-        IconFolderClean2=${IconFolderClean1##*/}
-        if [ "$IconFolderClean2" != "" ]; then
-            if [[ "$XDG_CURRENT_DESKTOP" = *"Cinnamon" ]]; then
-                dconf write /org/cinnamon/desktop/interface/icon-theme "'$IconFolderClean2'"
-            elif [[ "$XDG_CURRENT_DESKTOP" == "XFCE" ]] || [[ "$XDG_SESSION_DESKTOP" == "xfce" ]]; then
-                xfconf-query -c xsettings -p /Net/IconThemeName -s "$IconFolderClean2"
-            else
-                [ -n "$(dconf read /org/gnome/desktop/interface/color-scheme)" ] && dconf write /org/gnome/desktop/interface/icon-theme "'$IconFolderClean2'"
-            fi
-        fi
-
         exit 0
     fi
 
     # Light theme configuration
     if ! is_dark_theme && [ "$KvantumTheme" != "theme=BigAdwaitaRound" ]; then
-        # Configure GTK4 light theme
-        if [[ "$XDG_CURRENT_DESKTOP" = *"Cinnamon" ]] || [[ "$XDG_CURRENT_DESKTOP" == "XFCE" ]] || [[ "$XDG_SESSION_DESKTOP" == "xfce" ]]; then
-            dconf write /org/gnome/desktop/interface/color-scheme "'default'"
-        fi
-        
-        # Get icon theme based on desktop environment
-        if [[ "$XDG_CURRENT_DESKTOP" == "XFCE" ]] || [[ "$XDG_SESSION_DESKTOP" == "xfce" ]]; then
-            IconTheme="$(xfconf-query -c xsettings -p /Net/IconThemeName 2>/dev/null)"
-        else
-            IconTheme="$(dconf read /org/gnome/desktop/interface/icon-theme)"
-        fi
-        
         mkdir -p ~/.config/Kvantum/
         echo '[General]
     theme=BigAdwaitaRoundGtk' > ~/.config/Kvantum/kvantum.kvconfig
 
         # Copy the configuration file for the light theme
         cp -f /usr/share/sync-kde-and-gtk-places/biglinux ~/.config/kdeglobals
-
-        # Process and set light icon theme
-        IconThemeWithoutDark="$(echo $IconTheme | sed 's|-dark||gi;s|dark||gi')"
-        IconFolder="$(ls -d /usr/share/icons/*/ ~/.local/share/icons/*/ 2> /dev/null | grep -vi dark | grep -im1 "/${IconThemeWithoutDark//\'/}")"
-        IconFolderClean1=${IconFolder%/}
-        IconFolderClean2=${IconFolderClean1##*/}
-        if [ "$IconFolderClean2" != "" ]; then
-            if [[ "$XDG_CURRENT_DESKTOP" = *"Cinnamon" ]]; then
-                dconf write /org/cinnamon/desktop/interface/icon-theme "'$IconFolderClean2'"
-            elif [[ "$XDG_CURRENT_DESKTOP" == "XFCE" ]] || [[ "$XDG_SESSION_DESKTOP" == "xfce" ]]; then
-                xfconf-query -c xsettings -p /Net/IconThemeName -s "$IconFolderClean2"
-            else
-                [ -n "$(dconf read /org/gnome/desktop/interface/color-scheme)" ] && dconf write /org/gnome/desktop/interface/icon-theme "'$IconFolderClean2'"
-            fi
-        fi
 
         exit 0
     fi
